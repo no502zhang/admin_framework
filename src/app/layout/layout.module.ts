a2e8f03b5247f90809c5cf35aa9a5dffc455ed88
no502zhang <no502zhang@163.com>
2018-10-30 01:23:36 +0800

    拆分sidebar失败, 干脆不分组件了

--- src/app/layout/layout.module.ts
+++ src/app/layout/layout.module.ts
@@ -3,15 +3,12 @@ import { NgModule } from '@angular/core';
 import { SharedModule } from '../shared/shared.module';
 
 import { LayoutDefaultComponent } from './default/layout-default.component';
-import { LayoutHeaderComponent } from './default/header/layout-header.component';
-import { LayoutSidebarComponent } from './default/sidebar/layout-sidebar.component';
-import { LayoutContentComponent } from './default/content/layout-content.component';
 import { LayoutFullscreenComponent } from './fullscreen/layout-fullscreen.component';
 
 @NgModule({
   imports: [
     SharedModule
   ],
-  declarations: [LayoutDefaultComponent, LayoutHeaderComponent, LayoutSidebarComponent, LayoutContentComponent, LayoutFullscreenComponent,]
+  declarations: [LayoutDefaultComponent, LayoutFullscreenComponent,]
 })
 export class LayoutModule { }
