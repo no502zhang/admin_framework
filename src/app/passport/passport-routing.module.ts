2bbb7e2a9bfd4e212e6f4af363dd0ff3a493e7f5
no502zhang <no502zhang@163.com>
2018-11-01 19:56:21 +0800

    注册页面

--- src/app/passport/passport-routing.module.ts
+++ src/app/passport/passport-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 
 const dashboardRoutes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
 ];
 
 @NgModule({
--- src/app/passport/passport.module.ts
+++ src/app/passport/passport.module.ts
@@ -5,13 +5,14 @@ import { SharedModule } from '../shared/shared.module';
 import { PassportRoutingModule } from './passport-routing.module';
 
 import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 
 @NgModule({
   imports: [
     SharedModule,
     PassportRoutingModule
   ],
-  declarations: [LoginComponent],
-  exports: [LoginComponent]
+  declarations: [LoginComponent, RegisterComponent],
+  exports: [LoginComponent, RegisterComponent]
 })
 export class PassportModule { }
